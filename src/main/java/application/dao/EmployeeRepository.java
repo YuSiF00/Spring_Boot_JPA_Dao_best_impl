4a8a01e09459c8553abb48c8b7e82cca4b8437d8
yusif00 <yusifmemmedov553@gmail.com>
2020-04-25 04:51:15 +0400

    using Jpa repository

---
+++ src/main/java/application/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package application.dao;
+
+import application.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+}
---
+++ src/main/java/application/service/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+package application.service;
+
+import application.dao.EmployeeRepository;
+import application.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+
+        Optional<Employee> employee = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+        if (employee.isPresent()) {
+            theEmployee = employee.get();
+
+        } else {
+            throw new RuntimeException("Couldn't find employee with id - " + theId);
+        }
+
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+        employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+}
